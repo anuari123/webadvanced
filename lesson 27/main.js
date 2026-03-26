21f64a658496ec4aadf47ffc0589a8ca0e6df64d
anuari123 <anuarkiqmari1@gmail.com>
2026-03-26 19:07:58 +0100

    lesson27

---
+++ lesson 27/main.js
@@ -0,0 +1,46 @@
+function showMessage(){
+    alert("This is a message")
+}
+
+showMessage();
+
+function sum(number1, number2){
+return number1+number; 
+}
+
+console.log((24,4));
+
+var arrowFunction = () => alert("hello");
+
+arrowFunction();
+
+var arrowFunction = name => alert('hello$(name)');
+
+arrowFunction("maida");
+
+function dsFunction(){
+    var localVar="Digital School";
+
+    alert(localVar);
+}     
+
+dsFunction();
+
+var car={
+    name:"mercedes",
+    color:"red",
+    year: 2026,
+    kilometers:0,
+    startEngine: function(){
+        alert("vroom");
+    }
+}
+
+car.startEngine();
+
+var computer= new Object();
+
+computer.name="Lenvo";
+
+    
+
